0060453a296b6a2240de941941e0ba72da4cb285
Auh3b <ngaiyayec@yahoo.com>
2021-03-14 00:34:27 +0200

    updated to bs-card

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let url;
 const opt = document.getElementsByClassName("opt");
-const paraText = document.getElementById("parameter-text");
 const calculate = document.getElementById("calculate");
 const payable = document.getElementById("payable");
+
+let year = document.getElementById("year"),
+  parameter = document.getElementById("parameter"),
+  amount = document.getElementById("amount");
 
 async function getData(data) {
   const response = await fetch(data);
@@ -17,69 +20,74 @@ function getVehicle(d) {
 for (let i = 0; i < opt.length; i++) {
   opt[i].addEventListener("click", () => {
     url = getVehicle(opt[i].textContent);
+    let optLabel;
+
     if (opt[i].textContent === "Car") {
-      paraText.textContent = "Engine Capacity";
+      parameter.setAttribute("placeholder", "Enter Engine Capacity");
     } else if (opt[i].textContent === "Passanger") {
-      paraText.textContent = "Seating Capacity";
+      parameter.setAttribute("placeholder", "Enter Seating Capacity");
     } else if (opt[i].textContent === "Heavy Goods") {
-      paraText.textContent = "tonnage";
+      parameter.setAttribute("placeholder", "Enter tonnage");
     } else {
-      paraText.textContent = "";
+      parameter.setAttribute("placeholder", "");
     }
-    console.log(url);
+
+    getData(url)
+      .then((data) => {
+        calculate.addEventListener("click", () => {
+          // get parameter category
+          let category1;
+          data.forEach((d) => {
+            if (
+              d.cat1_min <= parameter.value &&
+              parameter.value <= d.cat1_max
+            ) {
+              category1 = d.cat1;
+            }
+          });
+
+          // get age category
+
+          let category2;
+          let baseDate = new Date();
+          let today = baseDate.getFullYear();
+          let age = today - parseInt(year.value);
+
+          data.forEach((d) => {
+            if ((d.cat2_min <= age) & (d.cat2_max >= age)) {
+              category2 = d.cat2;
+            }
+          });
+
+          // get array
+
+          let calArr = data.filter(
+            (d) => (d.cat1 == category1) & (d.cat2 == category2)
+          );
+
+          // calculate total custom
+
+          let totCustom;
+          let total =
+            amount.value * calArr[0].import_duty +
+            amount.value * calArr[0].import_excise +
+            amount.value * calArr[0].import_vat;
+
+          totCustom = total;
+
+          payable.innerHTML = `
+                          <span class="display-4">
+                            MWK ${$.number(totCustom)}
+                          </span>`;
+          clearFields();
+        });
+      })
+      .catch((err) => console.log(err));
   });
 }
 
-getData(url)
-  .then((data) => {
-    let year = document.getElementById("year"),
-      parameter = document.getElementById("parameter"),
-      amount = document.getElementById("amount");
-
-    calculate.addEventListener("click", () => {
-      // get parameter category
-      let category1;
-      data.forEach((d) => {
-        if (d.cat1_min <= parameter.value && parameter.value <= d.cat1_max) {
-          category1 = d.cat1;
-        }
-      });
-
-      // get age category
-
-      let category2;
-      let baseDate = new Date();
-      let today = baseDate.getFullYear();
-      let age = today - parseInt(year.value);
-
-      data.forEach((d) => {
-        if ((d.cat2_min <= age) & (d.cat2_max >= age)) {
-          category2 = d.cat2;
-        }
-      });
-
-      // get array
-
-      let calArr = data.filter(
-        (d) => (d.cat1 == category1) & (d.cat2 == category2)
-      );
-
-      // calculate total custom
-
-      let totCustom;
-      let total =
-        amount.value * calArr[0].import_duty +
-        amount.value * calArr[0].import_excise +
-        amount.value * calArr[0].import_vat;
-
-      totCustom = total;
-      // // console.log(totCustom);
-
-      payable.innerHTML = `<div>
-                        <span class="p-4 text-light bg-info">
-                          MWK ${$.number(totCustom)}
-                        </span>
-                        </div>`;
-    });
-  })
-  .catch((err) => console.log(err));
+function clearFields() {
+  year.value = "";
+  parameter.value = "";
+  amount.value = "";
+}
